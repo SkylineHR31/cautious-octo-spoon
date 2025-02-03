fa7a35770c9a472264e90d0274915903871714a4
Skyline_hr_31 <skyline_hr_31@outlook.com>
2025-02-03 19:02:47 +0200

    added HW5.2

---
+++ HW1/HW6/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+try
+{
+    Console.WriteLine("Enter number:");
+    string input = Console.ReadLine();
+    double number = Convert.ToDouble(input);
+    Console.WriteLine($"You entered the number: {number}");
+}
+catch (FormatException)
+{
+    Console.WriteLine("Error: the entered value is not a number.");
+}
