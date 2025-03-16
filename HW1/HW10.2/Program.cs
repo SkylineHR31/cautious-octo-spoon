c29614a33aa0d78d5a1a3b4b62b2dbcdb111d2a9
Skyline_hr_31 <skyline_hr_31@outlook.com>
2025-03-16 21:01:22 +0200

    added HW10.2

---
+++ HW1/HW10.2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+
+DateTime now = DateTime.Now;
+int counter = 1;
+StringBuilder sb = new StringBuilder("Report ", 50);
+sb.Append($"{now.ToString("yyyy-MM-dd")} \n");
+sb.Append("Events:\n");
+sb.ToString();
+Console.WriteLine(sb);
+
+while (true)
+{
+    string input = Console.ReadLine();
+    if (input == "exit" || input == null) break;
+    sb.Append($"{input} \n");
+    counter++;
+}
+
+sb.ToString();
+
+Console.WriteLine(sb);
