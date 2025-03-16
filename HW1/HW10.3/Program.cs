76f980b92b398712dc774a0e643f56416258c425
Skyline_hr_31 <skyline_hr_31@outlook.com>
2025-03-16 21:09:34 +0200

    added HW 10.3

---
+++ HW1/HW10.3/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Enter a string with words separated by commas: ");
+string input = Console.ReadLine();
+
+if (string.IsNullOrEmpty(input))
+{
+    Console.WriteLine("Invalid input. Please enter a valid string.");
+    return;
+}
+Console.WriteLine($"String without spaces: {input.Replace(" ", "")}");
