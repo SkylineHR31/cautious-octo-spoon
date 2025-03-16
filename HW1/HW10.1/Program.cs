afa1ed9670292f2503c98b9c91a3031d1be20f75
Skyline_hr_31 <skyline_hr_31@outlook.com>
2025-03-16 20:44:41 +0200

    added 10.1 HW

---
+++ HW1/HW10.1/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Enter full name: ");
+string fullName = Console.ReadLine();
+bool isEqual = false;
+
+string[] parsedName = fullName.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+if (parsedName.Length > 1)
+{
+    for (int i = 0; i < parsedName.Length - 1; i++)
+    {
+        if (char.ToUpper(parsedName[i][0]) == char.ToUpper(parsedName[i + 1][0]))
+        {
+            isEqual = true;
+            break;
+        }
+    }
+}
+
+if (isEqual)
+{
+    Console.WriteLine("First letter in both words are equal");
+} else Console.WriteLine("First letter in both words are NOT equal");
