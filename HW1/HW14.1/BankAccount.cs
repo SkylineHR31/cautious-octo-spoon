61cec0eb1e0ac5ad0f3be4b100c2c8858a02db71
Skyline_hr_31 <skyline_hr_31@outlook.com>
2025-03-17 21:31:20 +0200

    added HW 14.1

---
+++ HW1/HW14.1/BankAccount.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW14._1
+{
+    internal class BankAccount
+    {
+        public string Name { get; }
+        private double balance;
+
+        public double Balance
+        {
+            get { return balance; }
+            private set { balance = value < 0 ? 0 : value; }
+        }
+
+        public BankAccount(string name, double initialBalance)
+        {
+            Name = name ?? throw new ArgumentNullException(nameof(name), "Account must be defined");
+            Balance = initialBalance;
+        }
+
+        public void Deposit(double amount)
+        {
+            if (amount > 0)
+            {
+                Balance += amount;
+                Console.WriteLine($"{amount} deposited into {Name}. New balance: {Balance}");
+            }
+            else Console.WriteLine("Deposit amount must be positive");
+        }
+
+        public void Withdrawal(double amount)
+        {
+            if (amount > 0 && amount <= Balance)
+            {
+                Balance -= amount;
+                Console.WriteLine($"{amount} withdrawn from {Name}. New balance: {Balance}");
+            }
+            else Console.WriteLine("Invalid withdrawal amount");
+        }
+    }
+}
---
+++ HW1/HW14.1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Principal;
+
+namespace HW14._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account = new BankAccount("Account", 100.00);
+            BankAccount swissAccount = new BankAccount("SwissAccount", 1000000.00);
+
+            account.Withdrawal(20);
+            Console.WriteLine("Account balance is: " + account.Balance);
+
+            swissAccount.Deposit(200);
+            Console.WriteLine("swissAccount balance is: " + swissAccount.Balance);
+        }
+    }
+}
